7e9a9c7e475cf1cb0ac732a32c44e23a768991c0
Irma J Mejia <ijanettmejia@outlook.com>
2019-05-04 03:05:18 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -22,11 +22,11 @@ def end_game(card_total)
   puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
-def initial_round(sum)
-  2.times do
-    deal_card
-  end
-  display_card_total
+def initial_round
+  card_one = deal_card
+  card_two = deal_card
+  card_total = card_one + card_two
+  display_card_total(card_total)
 end
 
 def hit?
