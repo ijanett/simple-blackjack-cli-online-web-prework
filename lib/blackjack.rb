8b072de2d80708b94fa4ac9cec910e056dad7a3f
Irma J Mejia <ijanettmejia@outlook.com>
2019-05-04 03:33:28 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -55,8 +55,8 @@ def runner
   welcome
   total = initial_round
   until total > 21
-    total = hit? (total)
-    display_card_total (total)
+    total = hit?
+    display_card_total
   end
-end_game (total)
+end_game
 end
